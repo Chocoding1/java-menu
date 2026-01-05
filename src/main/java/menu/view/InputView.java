01eae4e2e4ad4fbc2a79109f23378b8ffb6e55b7
Chocoding1 <galmeagi2@naver.com>
2026-01-05 20:48:13 +0900

    Refactor: 개행 문자 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -6,7 +6,7 @@ import menu.model.Coach;
 public class InputView {
 
     private static final String READ_COACH_NAME_NOTICE = "코치의 이름을 입력해 주세요. (, 로 구분)";
-    private static final String READ_FORBIDDEN_MENU_NOTICE = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+    private static final String READ_FORBIDDEN_MENU_NOTICE = "%s(이)가 못 먹는 메뉴를 입력해 주세요.%n";
 
     public String readCoachName() {
         System.out.println(READ_COACH_NAME_NOTICE);
